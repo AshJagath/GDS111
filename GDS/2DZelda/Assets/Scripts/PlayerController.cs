43479b673bc4037552a2f3a4c053ebf869c65321
AshJagath <ash.stevens.chat@gmail.com>
2015-07-16 01:23:38 +1000

    blockout V1 initial commit
    small and standard room objects created
    simple moving character sprite, not colliding properly

---
+++ GDS/2DZelda/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private float HInput;
+	private float VInput;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void FixedUpdate(){
+
+
+
+
+		HInput = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		VInput = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+	
+		gameObject.transform.position += Vector3.up * VInput;
+		gameObject.transform.position += Vector3.right * HInput;
+
+	}
+
+	void Move(){
+
+		if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
+			
+			rigidbody2D.MovePosition (Vector2.up * (speed / 1000) *  Time.deltaTime );
+			
+		}
+
+	}
+
+
+}
