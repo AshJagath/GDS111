462c2b77327764be8ba277c8a8546bf0b35c8a83
ash <ash.stevens.chat@gmail.com>
2015-07-29 13:27:56 +1000

    added tile, building rooms
    standardized the floor tile to 1 unit

---
+++ GDS/2DZelda/Assets/Scripts/Floor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	[ContextMenu("Reposition")]
+	void Reposition()
+	{
+		var localPosition = transform.localPosition;
+		transform.localPosition = new Vector3 (Mathf.RoundToInt (localPosition.x), Mathf.RoundToInt (localPosition.y), Mathf.RoundToInt (localPosition.z));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
